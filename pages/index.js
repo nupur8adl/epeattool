a23ef30523b9622e395cb97052028ac0a68c9346
nupur8adl <nupur.joshi@adlventures.com>
2025-01-21 12:27:49 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import AssessmentTool from '../AssessmentTool';
+
+export default function Home() {
+  return (
+    <div className="container mx-auto py-8">
+      <AssessmentTool />
+    </div>
+  );
+}
